0ee21bd6650e4c8ea2dfd461b7d8771f8d777d7d
practicalcodewishshiva <practicalcodewithshiva@gmail.com>
2026-03-04 07:20:10 +0530

    initial changes

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-const a =10
+// Project -- safe place store ---- Github -- Prathi comment and prathi cod changes track cheyali 
+
+// 20 teams track 
+
+
+// clone? 
+
+
+// github account lo project me system lo clone 
+
+
+// git clone "githuburl"
+
+
+
+// 100%
+// git add .
+// git commit -m "worked on login page authentication"
+// git push
+
+// git merge 
+
+// git pull 
+
