dfbfba1cadfa52789260612e4736e2337bbed812
practicalcodewishshiva <practicalcodewithshiva@gmail.com>
2026-03-06 07:18:40 +0530

    feat:Worked on ui changes

---
+++ MakeMytrip/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      Button 
+      <Button variant="contained">Hello world </Button>
+      flights | Hotels | Railways |
+    </>
+  );
+}
+
+export default App;
